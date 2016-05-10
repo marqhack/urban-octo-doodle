da23de000618a98a7ef187a13645f531ad0e1025
marqhack <marqhack@gmail.com>
2016-05-10 18:08:56 -0400

    Okay...

---
+++ anchor.cpp
@@ -0,0 +1,8 @@
+/*
+ * Mike's makefile requires a .cpp file in the top level directory, but I want to
+ * restructure everything into its own folder in separte files
+ * I think I can just include everything in the top level file? 
+ * 
+*/
+
+#include "engine/engine.cpp" 
